cf7f483e7e04faed5a92690de4ac6ab65e1819c5
Kartik Kumar <me@kartikkumar.com>
2012-02-03 09:16:01 +0000

    Commit of corrections to Earth-orbiting satellite example.
    git-svn-id: file:///srv/svn/repos/tudatExampleApplications/trunk@13 c1df7300-1411-11e2-9e97-3139bfb8dd05

--- EarthOrbitingSatelliteExample/CMake/FindTudat.cmake
+++ EarthOrbitingSatelliteExample/CMake/FindTudat.cmake
@@ -53,7 +53,7 @@ else (TUDAT_INCLUDE_DIR)
   find_path(TUDAT_BASE_PATH NAMES tudatVersion.h
       PATHS
       ${PROJECT_SOURCE_DIR}/External
-      ${PROJECT_SOURCE_DIR}/../../../../tudat/trunk/branches/packages/
+      ${PROJECT_SOURCE_DIR}/../../../../tudat/branches/packages/
       ${PROJECT_SOURCE_DIR}/../../../../tudat/trunk
       ${PROJECT_SOURCE_DIR}/../../trunk
       ${CMAKE_INSTALL_PREFIX}/include
