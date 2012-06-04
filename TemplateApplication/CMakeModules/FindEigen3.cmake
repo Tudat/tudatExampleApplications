5d4645ff97bc075154f5aa054282fabd702fed52
Alexander Ronse <hidden@hidden.com>
2012-06-04 12:58:47 +0000

    Updated FindEigen3.cmake module
    git-svn-id: file:///srv/svn/repos/tudatExampleApplications/trunk@8 c1df7300-1411-11e2-9e97-3139bfb8dd05

--- TemplateApplication/CMakeModules/FindEigen3.cmake
+++ TemplateApplication/CMakeModules/FindEigen3.cmake
@@ -64,7 +64,8 @@ else (EIGEN3_INCLUDE_DIR)
   find_path(EIGEN3_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
       PATHS
       ${PROJECT_SOURCE_DIR}/External
-	  ${PROJECT_SOURCE_DIR}/../../../tudatcore/trunk
+	  ${PROJECT_SOURCE_DIR}/../../tudatCore
+	  ${PROJECT_SOURCE_DIR}/../../../tudatCore/trunk
       ${CMAKE_INSTALL_PREFIX}/include
       ${KDE4_INCLUDE_DIR}
       PATH_SUFFIXES eigen3 eigen
