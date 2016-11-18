410e84ae5aee89757d7db9f65b48ad3711a1144a
mvandenbroeck <m.m.h.vandenbroeck@student.tudelft.nl>
2016-11-18 16:33:00 +0100

    djd

---
+++ satellitePropagatorExamples/SatellitePropagatorExamples-build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/tudatBundle_mvdb/tudatExampleApplications/satellitePropagatorExamples/SatellitePropagatorExamples
+# Build directory: C:/tudatBundle_mvdb/tudatExampleApplications/satellitePropagatorExamples/SatellitePropagatorExamples-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
